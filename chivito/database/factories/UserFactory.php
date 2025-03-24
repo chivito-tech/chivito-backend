b3a92aab86d57496399e759d0b234a17790a922a
gabriel337 <g.david.lopezruiz@gmail.com>
2025-03-24 12:25:25 -0400

    feat(chi-19): created user migraiton and seeder

--- chivito/database/factories/UserFactory.php
+++ chivito/database/factories/UserFactory.php
@@ -24,11 +24,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => 'Test1234',
+            'phone_number' => $this->faker->phoneNumber(),
+            'photo' => $this->faker->imageUrl(200, 200, 'people', true, 'User'),
             'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
