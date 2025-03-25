590e43c71b8fe4d09e2f65d7ae59947278d8dd77
luiscolon0426 <lusantiago@protonmail.com>
2025-03-25 16:34:23 -0400

    added log in post

---
+++ chivito/app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'id' => $user->id,
+            'name' => $user->first_name . ' ' . $user->last_name,
+            'email' => $user->email,
+            'photo' => $user->photo ?? null,
+        ]);
+    }
+}
--- chivito/routes/api.php
+++ chivito/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/customers', [UserController::class, "getAllCustomers"]);
 Route::post('/createCustomer', [UserController::class, "create"]);
+
+
+Route::post('/login', [AuthController::class, 'login']);
